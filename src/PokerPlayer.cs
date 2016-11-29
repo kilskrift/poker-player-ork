9a09d5ae4b6691a91bb0e76f8108f870f756ca5f
Olov <gulliksson@gmail.com>
2016-11-29 14:30:17 +0100

    Renamed GreedyBet to FoldAlways fallback strategy, will fold iff no set or 11+ card in hand

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -55,92 +55,96 @@ namespace Nancy.Simple
                     return gameState.minimum_raise;
                 }
 
-                return GreedyBet(gameState, player);
+                return FoldAlways(gameState, player);
             }
             catch (Exception ex)
             {
-                Console.Error.WriteLine("exception, using GreedyBet Instead"+ ex);
+                Console.Error.WriteLine("exception, using FoldAlways Instead"+ ex);
 
-                return GreedyBet(gameState, gameState.players[gameState.in_action]);
+                return FoldAlways(gameState, gameState.players[gameState.in_action]);
             }
             //TODO: Use this method to return the value You want to bet
         }
 
-        private static int GreedyBet(GameState gameState, GameState.player player)
+        private static int FoldAlways(GameState gameState, GameState.player player)
         {
-            if (gameState.community_cards.Length >= 3)
-            {
-                var ourCards = player.hole_cards;
-                var ourCardList = ourCards.ToList();
-                ourCardList.AddRange(gameState.community_cards);
+            //if (gameState.community_cards.Length >= 3)
+            //{
+            //    var ourCards = player.hole_cards;
+            //    var ourCardList = ourCards.ToList();
+            //    ourCardList.AddRange(gameState.community_cards);
 
-                var ourRanks = ourCardList.Select(y => y.rank);
-                var ourGroups = ourRanks.GroupBy(y => y);
+            //    var ourRanks = ourCardList.Select(y => y.rank);
+            //    var ourGroups = ourRanks.GroupBy(y => y);
 
 
-                if (gameState.community_cards.Length == 3)
-                {
-                    if (ourGroups.Count() == 2)
-                    {
-                        return 153;
-                    }
+            //    Console.Error.WriteLine("exception, using FoldAlways Instead" + ex);
 
-                    if (ourGroups.Count() == 3)
-                    {
-                        return 13;
-                    }
-                    //
-                    return 3;
-                }
+            //    if (gameState.community_cards.Length == 3)
+            //    {
+            //        if (ourGroups.Count() == 2)
+            //        {
+            //            return 153;
+            //        }
 
-                if (gameState.community_cards.Length == 4)
-                {
-                    if (ourGroups.Count() == 2)
-                    {
-                        return 145;
-                    }
+            //        if (ourGroups.Count() == 3)
+            //        {
+            //            return 13;
+            //        }
+            //        //
+            //        return 3;
+            //    }
 
-                    if (ourGroups.Count() == 3)
-                    {
-                        return 114;
-                    }
+            //    if (gameState.community_cards.Length == 4)
+            //    {
+            //        if (ourGroups.Count() == 2)
+            //        {
+            //            return 145;
+            //        }
 
-                    if (ourGroups.Count() == 4)
-                    {
-                        return 14;
-                    }
-                    //
-                    return 4;
-                }
+            //        if (ourGroups.Count() == 3)
+            //        {
+            //            return 114;
+            //        }
 
-                if (gameState.community_cards.Length == 5)
-                {
-                    if (ourGroups.Count() == 2)
-                    {
-                        return 155;
-                    }
+            //        if (ourGroups.Count() == 4)
+            //        {
+            //            return 14;
+            //        }
+            //        //
+            //        return 4;
+            //    }
 
-                    if (ourGroups.Count() == 3)
-                    {
-                        return 145;
-                    }
+            //    if (gameState.community_cards.Length == 5)
+            //    {
+            //        if (ourGroups.Count() == 2)
+            //        {
+            //            return 155;
+            //        }
 
-                    if (ourGroups.Count() == 4)
-                    {
-                        return 105;
-                    }
-                    //
-                    return 5;
-                }
+            //        if (ourGroups.Count() == 3)
+            //        {
+            //            return 145;
+            //        }
 
-                return 1;
-            }
-            else
-            {
-                Console.Error.WriteLine("GreedyBet - minimum_raise"+ gameState.minimum_raise);
+            //        if (ourGroups.Count() == 4)
+            //        {
+            //            return 105;
+            //        }
+            //        //
+            //        return 5;
+            //    }
 
-                return gameState.minimum_raise;
-            }
+            //    return 1;
+            //}
+            //else
+            //{
+            //    Console.Error.WriteLine("FoldAlways - minimum_raise: "+ gameState.minimum_raise);
+
+            //    return gameState.minimum_raise;
+            //}
+            Console.Error.WriteLine("FoldAlways - fold always: ");
+            return 0;
         }
 
         public static int Fold()
